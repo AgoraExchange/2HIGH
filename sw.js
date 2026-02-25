07614f19679af3896b4c248e81adbb6a7928d8d6
Agora <agoraexhq@gmail.com>
2026-02-24 17:00:48 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE='2high-v2';
+const ASSETS=['./','./index.html','./style.css','./script.js','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET') return;
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{
+    const copy=res.clone();
+    caches.open(CACHE).then(c=>c.put(e.request, copy)).catch(()=>{});
+    return res;
+  }).catch(()=>cached)));
+});
